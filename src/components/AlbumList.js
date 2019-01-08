140ef6be578956bbc9e66c89c08815ae85f7321f
JBobda <J.BobdaT@gmail.com>
2019-01-08 14:33:54 -0600

    Added The Album List component, the Album Detail component, the Card component, and the card component, also added more documentation about styling in react-native

---
+++ src/components/AlbumList.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react';
+import { View } from 'react-native';
+
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        //Makes an HTTP Request and receives a json response
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => response.json())
+        .then(data => this.setState({albums: data}));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    } 
+}
+
+export default AlbumList;
--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,36 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 //Make a component
-const Header = () => {
+const Header = (props) => {
     const { textStyle, viewStyle } = styles;
 
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
 
 const styles = {
     viewStyle: {
-        backGroundColor: '#F8F8F8'
+        backgroundColor: '#FFFF00',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+
+        //IOS Shadow Configuration
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+
+        //Android Shadow Configuration
+        elevation: 2,
+        position: 'relative'
     },
     textStyle: {
-        fontSize: 20
+        fontSize: 20,
+        color: 'black'
     }
 };
 
